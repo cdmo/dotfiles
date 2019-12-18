6b95de58cfa58297318e5c36d8849dc9cc53d76d
Charlie Morris <cdm32@psu.edu>
2019-12-18 14:11:00 -0500

    Specifies lint fix command a bit more

--- zshrc/scss.zsh
+++ zshrc/scss.zsh
@@ -1,1 +1,1 @@
-alias stylelint-fix="./node_modules/stylelint/bin/stylelint.js scss/*  --config ./.stylelintrc --fix"
+alias stylelint-fix="./node_modules/stylelint/bin/stylelint.js scss/base/.scss scss/extra/.scss scss/layout/*.scss scss/f5_psul.scss --config ./.stylelintrc --fix"
