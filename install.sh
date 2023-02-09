80b8f8ed2d22123f6878a6c1630bfa5e013f6007
Charlie Morris <charlie.morris@shopify.com>
2023-02-09 22:05:57 +0000

    Adds history as a cartridge

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+  fzf \
+  rcm \
+  silversearcher-ag \
+  tmux
+
+# install mcfly
+cartridge insert mcfly
+ln -sf ~/.data/cartridges/mcfly ~/.mcfly
+curl -fsSL https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sudo sh -s -- --git cantino/mcfly
+
+# add systemd timer for persisting mcfly history
+sudo cp machine/spin/persist-history.service /etc/systemd/system/persist-history.service
+sudo cp machine/spin/persist-history.timer /etc/systemd/system/persist-history.timer
+systemctl start persist-history.service
