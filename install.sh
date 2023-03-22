449081e08de74e4586a439bb302d40803df42c5b
Charlie Morris <charlie.morris@shopify.com>
2023-03-22 15:51:22 +0000

    Remove reference to folder that doesn't exist

--- install.sh
+++ install.sh
@@ -12,6 +12,6 @@ ln -sf ~/.data/cartridges/mcfly ~/.mcfly
 curl -fsSL https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sudo sh -s -- --git cantino/mcfly
 
 # add systemd timer for persisting mcfly history
-sudo cp machine/spin/persist-history.service /etc/systemd/system/persist-history.service
-sudo cp machine/spin/persist-history.timer /etc/systemd/system/persist-history.timer
+sudo cp spin/persist-history.service /etc/systemd/system/persist-history.service
+sudo cp spin/persist-history.timer /etc/systemd/system/persist-history.timer
 systemctl start persist-history.service
