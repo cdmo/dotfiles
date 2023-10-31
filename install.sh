045b8b4dbbfedc5ad0e48f9be0b5939caaf9e5f8
Charlie Morris <charlie.morris@shopify.com>
2023-10-31 10:10:47 -0400

    Cleaning up mess, adding cloc

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# install mcfly
-cartridge insert mcfly
-ln -sf ~/.data/cartridges/mcfly ~/.mcfly
-curl -fsSL https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sudo sh -s -- --git cantino/mcfly
+if [ "$SPIN" ]; then
+  # count lines of code
+  sudo apt-get install -y \
+  fzf \
+  silversearcher-ag \
+  tmux \
+  cloc
 
-# add systemd timer for persisting mcfly history
-sudo cp spin/persist-history.service /etc/systemd/system/persist-history.service
-sudo cp spin/persist-history.timer /etc/systemd/system/persist-history.timer
-systemctl start persist-history.service
+  # install mcfly
+  cartridge insert mcfly
+  ln -sf ~/.data/cartridges/mcfly ~/.mcfly
+  curl -fsSL https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sudo sh -s -- --git cantino/mcfly
+
+  # add systemd timer for persisting mcfly history
+  sudo cp spin/persist-history.service /etc/systemd/system/persist-history.service
+  sudo cp spin/persist-history.timer /etc/systemd/system/persist-history.timer
+  systemctl start persist-history.service
+
+  # symlink dotfiles
+  ln -sf ~/shopify-dotfiles/zshrc ~/.zshrc
+fi
